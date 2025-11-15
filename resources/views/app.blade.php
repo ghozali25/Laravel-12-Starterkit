45439fae1f13b462e085e952cefaa94bf0ab25d2
ghozali25 <ghozali.ahmd25@gmail.com>
2025-11-15 10:45:13 +0700

    add meta

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -12,9 +12,12 @@
         $setting = $page['props']['setting'] ?? null;
         $appName = $setting['nama_app'] ?? config('app.name', 'Laravel');
         $favicon = $setting['favicon'] ?? null;
+        $seo = $setting['seo'] ?? null;
+        $metaDescription = $seo['description'] ?? 'Aplikasi manajemen aset modern yang membantu mengelola inventaris, karyawan, dan proses operasional secara efisien.';
     @endphp
 
     <title inertia>{{ $appName }}</title>
+    <meta name="description" content="{{ $metaDescription }}">
 
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
